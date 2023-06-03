65b60749eeb39f13bfa0267cc322cfe334695a1e
Juan Cruz <juanespasan@gmail.com>
2023-06-03 00:24:33 -0300

    Test routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import "./App.css"
 import React from "react"
-import { Route, Router } from "wouter"
+import { Route } from "wouter"
 import SearchResults from "./components/SearchResults"
 import GifDetails from "./components/Details"
-import { createHashHistory } from 'history';
 import Menu from "./pages/Menu"
 import {RecentGifs} from "./components/RecentGifs"
 
 import ColorApp from "./components/ColorApp"
 
 function App() {
-	const hashHistory = createHashHistory();
 	return (
-		<Router history={hashHistory}>
-			<section className="bodyCenter">
-				<Menu />
-				<div className="container">
-					<Route path="/gif/:keyword" component={SearchResults} />
-				</div>
-				<Route path="/gif-react/" component={RecentGifs} />
-				<Route path="/gif/:keyword/:id" component={GifDetails} />
-			</section>
-		</Router>
+		<section className="bodyCenter">
+			<Menu />
+			<div className="container">
+				<Route path="/gif/:keyword" component={SearchResults} />
+			</div>
+			<Route path="/gif-react/" component={RecentGifs} />
+			<Route path="/gif/:keyword/:id" component={GifDetails} />
+			<Route>This is rendered when nothing above has matched</Route>
+		</section>
 	)
 }
 
