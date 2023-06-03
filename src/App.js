9270ecbf4a61d9dd1ad4654737633acbc90b8310
Juan Cruz <juanespasan@gmail.com>
2023-06-03 00:17:02 -0300

    Implementando Hash Router para reconocer la url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css"
 import React from "react"
-import { Route } from "wouter"
+import { Route, HashRouter } from "wouter"
 import SearchResults from "./components/SearchResults"
 import GifDetails from "./components/Details"
 import Menu from "./pages/Menu"
@@ -10,14 +10,16 @@ import ColorApp from "./components/ColorApp"
 
 function App() {
 	return (
-		<section className="bodyCenter">
-			<Menu />
-			<div className="container">
-				<Route path="/gif/:keyword" component={SearchResults} />
-			</div>
-			<Route path="/gif-react/" component={RecentGifs} />
-			<Route path="/gif/:keyword/:id" component={GifDetails} />
-		</section>
+		<HashRouter>
+			<section className="bodyCenter">
+				<Menu />
+				<div className="container">
+					<Route path="/gif/:keyword" component={SearchResults} />
+				</div>
+				<Route path="/gif-react/" component={RecentGifs} />
+				<Route path="/gif/:keyword/:id" component={GifDetails} />
+			</section>
+		</HashRouter>
 	)
 }
 
