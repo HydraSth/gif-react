a24bc0d73886b696da43c57442c8226197629fac
Juan Cruz <juanespasan@gmail.com>
2023-06-03 00:35:25 -0300

    Testing routes switch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import "./App.css"
-import React from "react"
-import { Route } from "wouter"
-import SearchResults from "./components/SearchResults"
-import GifDetails from "./components/Details"
-import Menu from "./pages/Menu"
-import {RecentGifs} from "./components/RecentGifs"
+import "./App.css";
+import React from "react";
+import { Route, Switch, Link } from "wouter";
+import SearchResults from "./components/SearchResults";
+import GifDetails from "./components/Details";
+import Menu from "./pages/Menu";
+import { RecentGifs } from "./components/RecentGifs";
 
-import ColorApp from "./components/ColorApp"
+import ColorApp from "./components/ColorApp";
 
 function App() {
-	return (
-		<section className="bodyCenter">
-			<Menu />
-			<div className="container">
-				<Route path="/gif/:keyword" component={SearchResults} />
-			</div>
-			<Route path="/gif-react/" component={RecentGifs} />
-			<Route path="/gif/:keyword/:id" component={GifDetails} />
-			<Route>This is rendered when nothing above has matched</Route>
-		</section>
-	)
+  return (
+    <section className="bodyCenter">
+      <Menu />
+      <div className="container">
+        <Switch>
+          <Route path="/gif/:keyword" component={SearchResults} />
+          <Route path="/gif/:keyword/:id" component={GifDetails} />
+          <Route path="/gif-react/" component={RecentGifs} />
+        </Switch>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
