039688cbe93b23b4293a0098b11f5cfd84d47cf3
Juan Cruz <juanespasan@gmail.com>
2023-06-03 00:50:08 -0300

    Routes check | Verify 404

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import "./App.css";
-import React from "react";
-import { Route, Switch, Link } from "wouter";
-import SearchResults from "./components/SearchResults";
-import GifDetails from "./components/Details";
-import Menu from "./pages/Menu";
-import { RecentGifs } from "./components/RecentGifs";
+import "./App.css"
+import React from "react"
+import { Route } from "wouter"
+import SearchResults from "./components/SearchResults"
+import GifDetails from "./components/Details"
+import Menu from "./pages/Menu"
+import {RecentGifs} from "./components/RecentGifs"
 
-import ColorApp from "./components/ColorApp";
+import ColorApp from "./components/ColorApp"
 
 function App() {
-  return (
-    <section className="bodyCenter">
-      <Menu />
-      <div className="container">
-        <Switch>
-          <Route path="/gif/:keyword" component={SearchResults} />
-          <Route path="/gif/:keyword/:id" component={GifDetails} />
-          <Route path="/gif-react/" component={RecentGifs} />
-        </Switch>
-      </div>
-    </section>
-  );
+	return (
+		<section className="bodyCenter">
+			<Menu />
+			<div className="container">
+				<Route path="/gif/:keyword" component={SearchResults} />
+			</div>
+			<Route path="/gif-react/" component={RecentGifs} />
+			<Route path="/" component={RecentGifs} />
+			<Route path="/gif/:keyword/:id" component={GifDetails} />
+		</section>
+	)
 }
 
-export default App;
+export default App
